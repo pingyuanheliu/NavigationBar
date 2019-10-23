c7e2ca6bdefe873a8ed5d1d4bef50525952c2967
LL <15817490772@139.com>
2019-10-23 15:41:49 +0800

    Add notes.

--- NavigationBar/NavigationBar/UIViewController+handle.h
+++ NavigationBar/NavigationBar/UIViewController+handle.h
@@ -10,10 +10,12 @@
 
 @interface UIViewController (handle)
 
+//设置导航栏透明度
 @property (nonatomic, assign) CGFloat navBarAlpha;
 
 + (CGFloat)cx_navTopHeight;
 
+//更新导航栏背景色
 - (void)updateNavBar:(CGFloat)alpha;
 
 @end
